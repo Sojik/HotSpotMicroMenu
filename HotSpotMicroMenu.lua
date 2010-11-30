6437a13afbcba2e83fa90cf0fa2cf113c5c16cf3
Sojik <Br545wnlU@Bg1&f>
2010-11-30 15:18:45 -0500

    Quest/Achievement WatchFrame functionality added. WatchFrameCollapseExpandButton moved to top right corner by default.
    Signed-off-by: Sojik <Br545wnlU@Bg1&f>

--- HotSpotMicroMenu.lua
+++ HotSpotMicroMenu.lua
@@ -4,9 +4,10 @@ X   HotSpotMicroMenu by Sojik    X
 local fmicro = CreateFrame("Frame","MicroMenuHolder",UIParent)
 
 local MicroButtons = {
-	CharacterMicroButton, SpellbookMicroButton, TalentMicroButton,        -- Here's the names of the buttons.
-	AchievementMicroButton, QuestLogMicroButton, SocialsMicroButton,      -- You can use them for lines 19 - 22.
-	PVPMicroButton, LFDMicroButton, MainMenuMicroButton, HelpMicroButton, -- Don't change anything in this list though.
+AchievementMicroButton, CharacterMicroButton, HelpMicroButton,  -- Here's the names of the buttons.
+LFDMicroButton, MainMenuMicroButton, PVPMicroButton,            -- You can use them for lines 19 - 22.
+QuestLogMicroButton, SocialsMicroButton, SpellbookMicroButton,  -- Don't change anything in this list though unless you
+TalentMicroButton, WatchFrameCollapseExpandButton,              -- don't want the WatchFrameCollapseExpandButton moved.
 }
 
 local function MoveMicroButtons(skinName)
@@ -17,10 +18,17 @@ local function MoveMicroButtons(skinName)
 		f:SetAlpha(0)
 	end
 	CharacterMicroButton:SetPoint("TOPLEFT", UIParent, "TOPLEFT", -4, 20)    -- These lines are what you can edit.
-	SpellbookMicroButton:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", 4, 20)   -- Change the button name before :SetPoint(...)
-	LFDMicroButton:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 0, 0)      -- to any of the names from lines 7, 8 or 9.
+	WatchFrameCollapseExpandButton:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", 0, 0)   -- Change the button name before :SetPoint(...) If you're going to put a micro button here I suggest changing it to ("TOPRIGHT", UIParent, "TOPRIGHT", 4, 20)
+	SpellbookMicroButton:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 0, 0)      -- to any of the names from lines 7, 8 or 9.
 	TalentMicroButton:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMRIGHT", 0, 0) -- Capitalization is important.
 end
+
+hooksecurefunc('WatchFrame_Update', function()    -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
+	if WatchFrame.collapsed then                  -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
+		WatchFrameHeader:Hide()                   -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
+		WatchFrameTitle:SetText("")               -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
+	end                                           -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
+end)                                              -- Remove these lines to remove the Quest/Achievement WatchFrame functionality.
 
 fmicro:RegisterEvent("PLAYER_ENTERING_WORLD")
 fmicro:SetScript("OnEvent", function()
